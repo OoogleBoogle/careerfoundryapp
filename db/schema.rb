431241f7842afa8947f439b91b834408c4329f28
OoogleBoogle <simattfield@gmail.com>
2015-07-09 14:12:09 -0700

    Added cost to db and pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709204618) do
+ActiveRecord::Schema.define(version: 20150709210611) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20150709204618) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "price"
+    t.string   "cost"
   end
 
 end
