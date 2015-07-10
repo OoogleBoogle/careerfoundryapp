446aaca8af9c0aaffa1591c7d6b186eec93f5c5b
OoogleBoogle <simattfield@gmail.com>
2015-07-09 17:33:07 -0700

    Added test products to the model

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
     resources :products
-    resourses :orders, only: [:index, :new, :show, :create]
+    resources :orders, only: [:index, :new, :show, :create]
   # Example resource route with options:
   #   resources :products do
   #     member do
