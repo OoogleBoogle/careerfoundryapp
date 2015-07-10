13803e42d182afb14303dbb41c86cde383f899e3
OoogleBoogle <simattfield@gmail.com>
2015-07-10 11:41:31 -0700

    Created orders and ubdated schema.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,3 @@
 class Product < ActiveRecord::Base
-	photo = Product.create(
-						:name => "Test photo", 
-						:description => "I created this in the Model!", 
-						:image_url => "/images/California%20Street.jpg", 
-						:cost => "£20")
-	photo2 = Product.create(
-						:name => "Test photo2", 
-						:description => "I created this in the Model!", 
-						:image_url => "/images/California%20Street.jpg", 
-						:cost => "£20")
+	has_many :orders
 end
