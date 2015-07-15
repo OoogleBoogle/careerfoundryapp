d1b6d8e217166ebf348fcf5e9e8b41dd4d06056c
OoogleBoogle <simattfield@gmail.com>
2015-07-15 10:00:08 -0700

    Products added to seed.rb.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,3 @@
 class Product < ActiveRecord::Base
 	has_many :orders
-	Product.create(:name => "Jetty", :description => "Lake Tahoe Jetty", :image_url => "/images/Jetty-carousel.jpg", :cost => "£20")
-	Product.create(:name => "Chicago", :description => "Chicago skyline", :image_url => "/images/chicago-carousel.jpg", :cost => "£30")
-	Product.create(:name => "San Francisco", :description => "View down a street in San Francisco", :image_url => "/images/california-street.jpg", :cost => "£30")
-	Product.create(:name => "Rue Bourbon", :description => "Classic New Orleans", :image_url => "/images/rue-bourbon.jpg", :cost => "£20")
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create(:name => "Jetty", :description => "Lake Tahoe Jetty", :image_url => "/images/Jetty-carousel.jpg", :cost => "£20")
+Product.create(:name => "Chicago", :description => "Chicago skyline", :image_url => "/images/chicago-carousel.jpg", :cost => "£30")
+Product.create(:name => "San Francisco", :description => "View down a street in San Francisco", :image_url => "/images/california-street.jpg", :cost => "£30")
+Product.create(:name => "Rue Bourbon", :description => "Classic New Orleans", :image_url => "/images/rue-bourbon.jpg", :cost => "£20")
