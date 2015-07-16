fa770b6d8c72a39f4c0db07d315db12344dba869
OoogleBoogle <simattfield@gmail.com>
2015-07-16 13:22:13 -0700

    Added sendgrid mailer.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,15 @@ class StaticPagesController < ApplicationController
   def landing_page
 	@products = Product.limit(2)
   end
+
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(
+  		:from => @email,
+  		:to => 'simattfield@gmail.com',
+  		:subject => "A contact message from #{@name}",
+  		:body => @message).deliver
+  end
 end
