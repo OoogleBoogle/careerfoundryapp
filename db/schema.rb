80e438e1877e0ce7506c36ddc9dd74a3e3eac5e3
OoogleBoogle <simattfield@gmail.com>
2015-07-17 11:30:30 -0700

    dropped previous User table and readded

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150715230625) do
+ActiveRecord::Schema.define(version: 20150717182743) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20150715230625) do
     t.string   "cost"
     t.string   "size"
     t.string   "orientation"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
