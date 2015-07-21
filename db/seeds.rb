5fafcab138e98bb6c5ae62a82dab8bd5344a9862
OoogleBoogle <simattfield@gmail.com>
2015-07-21 10:23:40 -0700

    Added order page as nav dropdown when user logged in.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,5 @@ Product.create(:name => "Jetty", :description => "Lake Tahoe Jetty", :image_url 
 Product.create(:name => "Chicago", :description => "Chicago skyline", :image_url => "/images/chicago-carousel.jpg", :cost => "£30", :orientation => "landscape")
 Product.create(:name => "Rue Bourbon", :description => "Classic New Orleans", :image_url => "/images/rue-bourbon.jpg", :cost => "£20", :orientation => "portrait")
 Product.create(:name => "Jetty", :description => "Lake Tahoe Jetty", :image_url => "/images/Jetty-carousel.jpg", :cost => "£20", :orientation => "landscape")
+
+Order.create(:user_id => 1, :product_id => 1)
