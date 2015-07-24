70082cf63c63758efff5f4d53eb57ad8aeda55e2
OoogleBoogle <simattfield@gmail.com>
2015-07-24 11:05:45 -0700

    Defined admin and non-admin permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,11 +5,12 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
       user ||= User.new # guest user (not logged in)
-    #   if user.admin?
+      if user.admin?
         can :manage, User, id: user.id
-    #   else
-    #     can :read, :all
-    #   end
+      else
+        can :read, [:products, :comments, :orders]
+        can :create, :comments
+      end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
