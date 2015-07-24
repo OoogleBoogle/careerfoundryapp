e00d7b90a8cede5a9fa3fb0684a70903f3a1b06d
OoogleBoogle <simattfield@gmail.com>
2015-07-24 11:52:58 -0700

    schema update?

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150722162317) do
+ActiveRecord::Schema.define(version: 20150724171137) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
