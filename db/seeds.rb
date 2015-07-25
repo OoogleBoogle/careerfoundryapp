dff66456551b2c846176ad18cd2333aec4e2f39d
OoogleBoogle <simattfield@gmail.com>
2015-07-24 19:12:11 -0700

    Added edit account page and styles pagination links

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-Product.create(:name => "Jetty", :description => "Lake Tahoe Jetty", :image_url => "/images/Jetty-carousel.jpg", :cost => "£20", :orientation => "landscape")
-Product.create(:name => "Chicago", :description => "Chicago skyline", :image_url => "/images/chicago-carousel.jpg", :cost => "£30", :orientation => "landscape")
-Product.create(:name => "San Francisco", :description => "View down a street in San Francisco", :image_url => "/images/california-street.jpg", :cost => "£30", :orientation => "portrait")
-Product.create(:name => "Rue Bourbon", :description => "Classic New Orleans", :image_url => "/images/rue-bourbon.jpg", :cost => "£20", :orientation => "portrait")
-Product.create(:name => "Chicago", :description => "Chicago skyline", :image_url => "/images/chicago-carousel.jpg", :cost => "£30", :orientation => "landscape")
-Product.create(:name => "San Francisco", :description => "View down a street in San Francisco", :image_url => "/images/california-street.jpg", :cost => "£30", :orientation => "portrait")
-Product.create(:name => "Jetty", :description => "Lake Tahoe Jetty", :image_url => "/images/Jetty-carousel.jpg", :cost => "£20", :orientation => "landscape")
-Product.create(:name => "Chicago", :description => "Chicago skyline", :image_url => "/images/chicago-carousel.jpg", :cost => "£30", :orientation => "landscape")
-Product.create(:name => "Rue Bourbon", :description => "Classic New Orleans", :image_url => "/images/rue-bourbon.jpg", :cost => "£20", :orientation => "portrait")
-Product.create(:name => "Jetty", :description => "Lake Tahoe Jetty", :image_url => "/images/Jetty-carousel.jpg", :cost => "£20", :orientation => "landscape")
-Product.create(:name => "Jetty", :description => "Lake Tahoe Jetty", :image_url => "/images/Jetty-carousel.jpg", :cost => "£20", :orientation => "landscape")
-Product.create(:name => "Chicago", :description => "Chicago skyline", :image_url => "/images/chicago-carousel.jpg", :cost => "£30", :orientation => "landscape")
-Product.create(:name => "San Francisco", :description => "View down a street in San Francisco", :image_url => "/images/california-street.jpg", :cost => "£30", :orientation => "portrait")
-Product.create(:name => "Rue Bourbon", :description => "Classic New Orleans", :image_url => "/images/rue-bourbon.jpg", :cost => "£20", :orientation => "portrait")
-Product.create(:name => "Chicago", :description => "Chicago skyline", :image_url => "/images/chicago-carousel.jpg", :cost => "£30", :orientation => "landscape")
-Product.create(:name => "San Francisco", :description => "View down a street in San Francisco", :image_url => "/images/california-street.jpg", :cost => "£30", :orientation => "portrait")
-Product.create(:name => "Jetty", :description => "Lake Tahoe Jetty", :image_url => "/images/Jetty-carousel.jpg", :cost => "£20", :orientation => "landscape")
-Product.create(:name => "Chicago", :description => "Chicago skyline", :image_url => "/images/chicago-carousel.jpg", :cost => "£30", :orientation => "landscape")
-Product.create(:name => "Rue Bourbon", :description => "Classic New Orleans", :image_url => "/images/rue-bourbon.jpg", :cost => "£20", :orientation => "portrait")
-Product.create(:name => "Jetty", :description => "Lake Tahoe Jetty", :image_url => "/images/Jetty-carousel.jpg", :cost => "£20", :orientation => "landscape")
+
+Product.create!([
+  {name: "Jetty", description: "Lake Tahoe Jetty", image_url: "/images/Jetty-carousel.jpg", cost: "£20", size: nil, orientation: "landscape"},
+  {name: "Chicago", description: "Chicago skyline", image_url: "/images/chicago-carousel.jpg", cost: "£30", size: nil, orientation: "landscape"},
+  {name: "San Francisco", description: "View down a street in San Francisco", image_url: "/images/california-street.jpg", cost: "£30", size: nil, orientation: "portrait"},
+  {name: "Rue Bourbon", description: "Classic New Orleans", image_url: "/images/rue-bourbon.jpg", cost: "£20", size: nil, orientation: "portrait"},
+  {name: "Chicago", description: "Chicago skyline", image_url: "/images/chicago-carousel.jpg", cost: "£30", size: nil, orientation: "landscape"},
+  {name: "San Francisco", description: "View down a street in San Francisco", image_url: "/images/california-street.jpg", cost: "£30", size: nil, orientation: "portrait"},
+  {name: "Jetty", description: "Lake Tahoe Jetty", image_url: "/images/Jetty-carousel.jpg", cost: "£20", size: nil, orientation: "landscape"},
+  {name: "Chicago", description: "Chicago skyline", image_url: "/images/chicago-carousel.jpg", cost: "£30", size: nil, orientation: "landscape"},
+  {name: "Rue Bourbon", description: "Classic New Orleans", image_url: "/images/rue-bourbon.jpg", cost: "£20", size: nil, orientation: "portrait"},
+  {name: "Jetty", description: "Lake Tahoe Jetty", image_url: "/images/Jetty-carousel.jpg", cost: "£20", size: nil, orientation: "landscape"},
+  {name: "Jetty", description: "Lake Tahoe Jetty", image_url: "/images/Jetty-carousel.jpg", cost: "£20", size: nil, orientation: "landscape"},
+  {name: "Chicago", description: "Chicago skyline", image_url: "/images/chicago-carousel.jpg", cost: "£30", size: nil, orientation: "landscape"},
+  {name: "San Francisco", description: "View down a street in San Francisco", image_url: "/images/california-street.jpg", cost: "£30", size: nil, orientation: "portrait"},
+  {name: "Rue Bourbon", description: "Classic New Orleans", image_url: "/images/rue-bourbon.jpg", cost: "£20", size: nil, orientation: "portrait"},
+  {name: "Chicago", description: "Chicago skyline", image_url: "/images/chicago-carousel.jpg", cost: "£30", size: nil, orientation: "landscape"},
+  {name: "San Francisco", description: "View down a street in San Francisco", image_url: "/images/california-street.jpg", cost: "£30", size: nil, orientation: "portrait"},
+  {name: "Jetty", description: "Lake Tahoe Jetty", image_url: "/images/Jetty-carousel.jpg", cost: "£20", size: nil, orientation: "landscape"},
+  {name: "Chicago", description: "Chicago skyline", image_url: "/images/chicago-carousel.jpg", cost: "£30", size: nil, orientation: "landscape"},
+  {name: "Rue Bourbon", description: "Classic New Orleans", image_url: "/images/rue-bourbon.jpg", cost: "£20", size: nil, orientation: "portrait"},
+  {name: "Jetty", description: "Lake Tahoe Jetty", image_url: "/images/Jetty-carousel.jpg", cost: "£20", size: nil, orientation: "landscape"}
+])
